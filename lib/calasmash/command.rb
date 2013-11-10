3284b1496b03b87c3890e43ee62042d588afe5ed
Alex Fish <alex@alexefish.com>
2013-11-10 21:54:05 +0000

    Adding Compiler to run xcodebuild command
    * Parsing options from the command

--- lib/calasmash/command.rb
+++ lib/calasmash/command.rb
@@ -13,14 +13,64 @@ module Calasmash
 
     class << self
 
-      # Public: executes a command.
       #
-      # args    - The actual commands to operate on. Can be as few as zero
-      #           arguments or as many as three.
+      # Execute a command with some arguments
+      # then figure out what we're supposed to be doing with
+      # the arguments
+      #
+      # @param  *args [Array] An Array of arguments to play with
+      #
+      # @return [type] [description]
       def execute(*args)
-        command = args.shift
+        return overview unless args.length > 1
 
-        puts command
+        options = parse(args)
+
+        # Compile the project
+        compile(options[:scheme])
+      end
+
+      #
+      # parse the arguments and act on them
+      # @param  args [Array] The arguments from execute
+      #
+      # @return [Hash] A hash containing all of our options
+      def parse(args)
+        options = {}
+        options[:tags] = []
+
+        OptionParser.new do |opt|
+         opt.on("-s","--scheme SCHEME","the scheme to build") do |tags|
+            options[:scheme] = tags
+          end
+
+          opt.on("-t","--tags TAGS","the tags to pass to Cucumber") do |tag_set|
+            options[:tags] << tag_set
+          end
+
+          opt.on('-i', "--ios OS", "iOS simulator version of the sdk to run e.g. 6.0 or 7.0") do |tags|
+            options[:ios] = tags
+          end
+        end.parse!
+
+        return options
+      end
+
+      def compile(scheme)
+        compiler = Calasmash::Compiler.new(scheme)
+        compiler.compile
+      end
+
+      #
+      # Outputs a nice helpful banner overview to STDOUT
+      #
+      def overview
+        s = "Usage: calasmash [OPTIONS]"
+        s << "\n  --tags -t the tags to pass to Cucumber"
+        s << "\n  --scheme -s the Xcode scheme to build"
+        s << "\n  --ios -i the iOS version to build with"
+
+        puts s
       end
 
     end
---
+++ lib/calasmash/compiler.rb
@@ -0,0 +1,91 @@
+# coding: utf-8
+require "open3"
+
+module Calasmash
+
+  #
+  # The calamsash compiler will compiles the Xcode project with the
+  # scheme it's told to compile with.
+  #
+  # @author [alexfish]
+  #
+  class Compiler
+
+    # Public: the Scheme the compiler is compiling
+    attr_accessor :scheme
+
+    def initialize(scheme)
+      @scheme = scheme
+    end
+
+    #
+    # The compiler's heart, executes the compiling with xcodebuild
+    #
+    #  @param  &complete [type] [description]
+    #
+    # Returns nothing because it completes with a complete block
+    def compile &complete
+
+      status = nil
+
+      xcode_command = "xcodebuild -workspace #{workspace} \
+                       -scheme #{@scheme} \
+                       -sdk iphonesimulator \
+                       CODE_SIGN_IDENTITY="" \
+                       CODE_SIGNING_REQUIRED=NO"
+
+      Open3.popen3 xcode_command do |stdin, out, err, wait_thr|
+        [out, err].each do |stream|
+          Thread.new do
+            until (line = stream.gets).nil? do
+              puts line
+            end
+          end
+        end
+        wait_thr.join
+        status = wait_thr.value.exitstatus
+      end
+
+      if status != 0
+        puts "Compilation failed: #{status}"
+        exit status
+      end
+    end
+
+    #
+    # Looks in the current directory for the workspace file and
+    # gets it's name
+    #
+    # @return [String] The name of the workspace file that was found
+    def workspace
+      "laterooms-ios.xcworkspace"
+    end
+
+  end
+
+end
+
+#   def compile
+#     puts "Compiling..."
+#
+#     status = nil
+#     xcode_command = "xcodebuild -workspace #{@options[:workspace]} -scheme #{@options[:scheme]} -sdk iphonesimulator CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO"
+#     Open3.popen3 xcode_command do |stdin, out, err, wait_thr|
+#
+#       [out, err].each do |stream|
+#         Thread.new do
+#           until (line = stream.gets).nil? do
+#             puts line
+#           end
+#         end
+#       end
+#
+#       wait_thr.join
+#       status = wait_thr.value.exitstatus
+#     end
+#
+#     if status != 0
+#       puts "Compilation failed: #{status}"
+#       exit status
+#     end
+#   end
