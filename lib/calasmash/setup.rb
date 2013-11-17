663696aa9845bf7219f7c6002f4093a5022b6657
Alex Fish <alex@alexefish.com>
2013-11-17 22:56:26 +0000

    Adding selected group method to setup

--- lib/calasmash/setup.rb
+++ lib/calasmash/setup.rb
@@ -30,9 +30,25 @@ module Calasmash
       def add_plist
         proj = XcodeProject::Project.new(xcodeproj)
         data = proj.read
-        data.main_group.children.each do |child|
-          p child.name
+        puts selected_group(data)
+      end
+
+      #
+      # Output the available groups to the console and
+      # let the user select one
+      #
+      # @param data [XcodeProject] The Xcode project to select a group from
+      #
+      # @return [XcodeProject::PBXGroup] The selected Xcode group
+      def selected_group(data)
+        puts "Selet a group for the calasmash server config plist:\n\n"
+        groups = {}
+        data.main_group.children.each_with_index do |child, i|
+          groups[i] = child
+          puts "#{i + 1}. #{child.name}"
         end
+        option = $stdin.gets.chomp
+        groups[option.to_i - 1]
       end
 
       def xcodeproj
